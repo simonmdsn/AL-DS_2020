978867e26308f01288e4455ea122227e0ace83f0
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2020-03-02 13:05:57 +0100

    week 10.

---
+++ Week 9/Week 10/Assignment 1.py
@@ -0,0 +1,7 @@
+"""
+Show that the worst-case running time of HEAPSORT is Ω(n log n)
+"""
+
+"""
+Træet er log n højt, og vi skal køre alle elementer igennem, da vi har med worst case at gøre derfor n log n. 
+"""
